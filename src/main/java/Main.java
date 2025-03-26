0a351cce97e69efc2167b0e2e93f8f4d0231e803
Dennis Ward <144068419+Maiar0@users.noreply.github.com>
2025-03-26 12:44:00 -0500

    initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,22 @@
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+
+public class Main {
+  public static void main(String[] args) {
+    /*TaskService taskService = TaskService.getInstance();
+    for(int i = 0; i < 5; i++){
+      Task task = new Task("Task " + i, "Description " + i);
+      taskService.addTask(task);
+    }
+    taskService.printTasks();
+    //taskService.getTask("001").setTaskId("006");
+    taskService.deleteTask("006");
+    taskService.printTasks();*/
+  }
+
+  // @Test
+  // void addition() {
+  //     assertEquals(2, 1 + 1);
+  // }
+}
